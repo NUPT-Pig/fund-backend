e84cdfb08ba9a3b5bf0fc95fd43eede569debc5a
anshun_zhu <nupt_b09040212@sina.com>
2020-12-09 22:30:40 +0800

    (test) [assert; get vo]
    {}

--- src/test/java/org/nuptpig/fundbackend/FundBackendApplicationTests.java
+++ src/test/java/org/nuptpig/fundbackend/FundBackendApplicationTests.java
@@ -4,13 +4,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.nuptpig.fundbackend.dao.FundRepository;
 import org.nuptpig.fundbackend.entity.Fund;
+import org.nuptpig.fundbackend.util.CommonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class FundBackendApplicationTests {
+public class FundBackendApplicationTests {
+
+    protected <T> T getVo(ResponseEntity responseEntity){
+        CommonResult commonResult = (CommonResult) responseEntity.getBody();
+        return (T) commonResult.getData();
+    }
 
     @Test
     void contextLoads() {
--- src/test/java/org/nuptpig/fundbackend/controller/FundControllerTest.java
+++ src/test/java/org/nuptpig/fundbackend/controller/FundControllerTest.java
@@ -1,31 +1,45 @@
 package org.nuptpig.fundbackend.controller;
 
 import org.junit.jupiter.api.Test;
+import org.nuptpig.fundbackend.FundBackendApplicationTests;
+import org.nuptpig.fundbackend.util.CommonResult;
+import org.nuptpig.fundbackend.vo.FundDetailResponse;
+import org.nuptpig.fundbackend.vo.PageableFundResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import org.assertj.core.api.Assertions;
 
-@ActiveProfiles("test")
-@SpringBootTest
-class FundControllerTest {
+
+class FundControllerTest extends FundBackendApplicationTests {
+
     @Autowired
     private FundController fundController;
 
     @Test
     void getFunds() {
-        ResponseEntity responseEntity = fundController.getFunds(1, 2);
-        System.out.println(responseEntity.getStatusCode());
-        System.out.println(responseEntity.getBody());
+        Long INIT_FUND_TOTAL = 5L;
+        ResponseEntity responseEntity = fundController.getFunds(20, 1);
+        Assertions.assertThat(responseEntity.getStatusCode().equals(HttpStatus.OK));
+        PageableFundResponse pageableFundResponse = getVo(responseEntity);
+        Assertions.assertThat(pageableFundResponse.getTotal().equals(INIT_FUND_TOTAL));
     }
 
     @Test
+    @Transactional
     void createFund() {
     }
 
     @Test
     void getFundByFundCode() {
-
+        String fundCode = "501010";
+        ResponseEntity responseEntity = fundController.getFundByFundCode(fundCode);
+        Assertions.assertThat(responseEntity.getStatusCode().equals(HttpStatus.OK));
+        FundDetailResponse fundDetailResponse = getVo(responseEntity);
+        Assertions.assertThat(fundDetailResponse.getFundCode().equals(fundCode));
     }
 
     @Test
